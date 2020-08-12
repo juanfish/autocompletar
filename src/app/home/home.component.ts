fe7d0f67e9222ca160fbbd9653ff791f5c034349
juanfish <juanfish@hotmail.com>
2020-08-12 08:40:56 -0400

    combos funcionales version 1

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef, Inject } from '@angular/core';
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { DOCUMENT } from "@angular/common";
-import { Observable, fromEvent } from 'rxjs';
+import { Observable, fromEvent, Subscription } from 'rxjs';
 import { debounceTime, map, distinctUntilChanged } from 'rxjs/operators';
 import { Subject } from 'rxjs';
 import { THIS_EXPR, ThrowStmt } from '@angular/compiler/src/output/output_ast';
@@ -15,12 +15,18 @@ import { THIS_EXPR, ThrowStmt } from '@angular/compiler/src/output/output_ast';
 export class HomeComponent implements OnInit {
   searchTextChanged = new Subject<string>();
   subscription: any;
-  txtConsulta: string;
-  txtTpCliente: string = '';
+  
+  // subscription: Subscription;
 
-  nombres = [];
+  txtConsulta: string;
+  txtTpCliente: string = 'N';
+  txtTitulo: string = '';
+
+  clientes = [];
+  titulos = [];
   arreglo = [];
   jsonDatosM: any;
+  jsonDatosM2: any;
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -36,12 +42,15 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  ngOnInit() {
+
+  }
+
   constructor(private http: HttpClient) {
 
   }
 
   getValues() {
-    
 
     let obs = this.http.post('http://la2K12.eastus.cloudapp.azure.com:8079/pview/PViewISAPI.dll/la/rest/TView/WBusca_DatosM',
       this.paramXXX,
@@ -53,40 +62,94 @@ export class HomeComponent implements OnInit {
       this.jsonDatosM.visible = true;
 
       this.arreglo = [];
-      this.nombres = [];
+      this.clientes = [];
       this.arreglo.push(...resultado['CadJson']['Ficha']);
 
       this.arreglo.forEach((elemento) => {
-        this.nombres.push(elemento["Nombreficha"])
+        this.clientes.push(elemento["Nombreficha"])
       })
-      console.table(this.nombres)
+  
 
     })
   }
 
-  ngOnInit() {
 
+  getValuesTit() {
+    
+    let obs = this.http.post('http://la2K12.eastus.cloudapp.azure.com:8079/pview/PViewISAPI.dll/la/rest/TView/WBusca_titulos',
+      this.paramXXX,
+      this.httpOptions);
+
+    return obs.subscribe(resultado => {
+
+      console.log("resultado titulo")
+      console.log(resultado)
+      this.jsonDatosM2 = resultado;
+      this.jsonDatosM2.visible = true;
+
+      this.arreglo = [];
+      this.titulos = [];
+      this.arreglo.push(...resultado['CadJson']['Tit']);
+
+      this.arreglo.forEach((elemento) => {
+        this.titulos.push(elemento["Nombretitulo"])
+      })
+      
+    })
+  }
+
+
+  subscribeCli(){
+    console.log('cliente')
     this.subscription = this.searchTextChanged.pipe(
       debounceTime(1000),
       distinctUntilChanged(),
       map(search => this.getValues())
     ).subscribe((res) => {
-      console.log(res);
+      // console.log(res);
     });
+
   }
 
   onChangeConsultaCliente(evt) {
-    this.paramXXX.param.Codigo = this.txtConsulta;
-    this.paramXXX.param.Tpcodigo = this.txtTpCliente;
-
-    this.searchTextChanged.next(evt.target.value);
+    console.log(evt.target)
+    if ((this.clientes == undefined) || (this.clientes.length === 0)) {      
+      this.paramXXX.param.Codigo = this.txtConsulta;
+      this.paramXXX.param.Tpcodigo = this.txtTpCliente;
+  
+      this.searchTextChanged.next(evt.target.value);
+    }
+    if (evt.target.value === ''){
+      this.clientes = [];
+    }
   }
 
-  onChangeConsultaTitulo(evt) {
-    this.paramXXX.param.Codigo = this.txtConsulta;
-    this.paramXXX.param.Tpcodigo = this.txtTpCliente;
+  subscribeTit(){
+    console.log('titulo')
 
-    // this.searchTextChanged.next(evt.target.value);
+    this.subscription = this.searchTextChanged.pipe(
+      debounceTime(700),
+      distinctUntilChanged(),
+      map(search => this.getValuesTit())
+    ).subscribe((res) => {
+      // console.log(res);
+    });
+
+  }
+
+
+  onChangeConsultaTitulo(evt) {
+    console.log(evt.target)
+    if ((this.titulos == undefined) || (this.titulos.length === 0)) {  
+    this.paramXXX.param.Codigo = this.txtTitulo;
+    this.paramXXX.param.Tpcodigo = '';
+
+    this.searchTextChanged.next(evt.target.value);
+    }
+    if (evt.target.value === ''){
+      this.titulos = [];
+    }
+
   }
 
   onRbChange(e){
@@ -94,5 +157,7 @@ export class HomeComponent implements OnInit {
     console.log(this.txtTpCliente);
   }
 
-
+  onClickButton(){
+    alert(this.txtConsulta);
+  }
 }
